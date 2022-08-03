56aa73a9ae1064a117e2ef08274f64587089877f
Sntu2132 <santhoshKumardsp@gmail.com>
2022-08-03 06:46:27 +0000

    blogList

---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+const UserInfo = () => (
+  <div className="container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+      alt="UserImage"
+      className="image"
+    />
+    <h1 className="heading">Wade Warren</h1>
+    <p className="description">Software Developer At UK</p>
+  </div>
+)
+
+export default UserInfo
